c908c9904fd0222d0b9662292ec6f3f4325c08fc
Петр Яненко <without1481@gmail.com>
2023-01-30 14:05:09 +0200

    initial commit

---
+++ Devote/DevoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevoteApp.swift
+//  Devote
+//
+//  Created by Алина on 30.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct DevoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
